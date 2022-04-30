ae958b6f5ca444287ed628972163c27348bdd661
MRPedrosa <104232967+MRPedrosa@users.noreply.github.com>
2022-04-30 19:58:57 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*Este programa imprime cuatro lineas por consola,
+y ademas tiene comentarios*/
+console.log("Este");
+//Este es un comentario
+console.log("algoritmo");
+//Este es otro comentario
+console.log("es");
+//Este es un cuarto comentario
+console.log("secuencial");
