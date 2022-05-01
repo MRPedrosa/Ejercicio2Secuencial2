02078953757be954c0a40398b92e9698bdb39527
MRPedrosa <104232967+MRPedrosa@users.noreply.github.com>
2022-04-30 22:05:20 -0300

    cambio en secuencia reemplazo por variable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 /*Este programa imprime cuatro lineas por consola,
 y ademas tiene comentarios*/
-console.log("Este");
+//console.log("Este");
 //Este es un comentario
-console.log("algoritmo");
+//console.log("algoritmo");
 //Este es otro comentario
-console.log("es");
+//console.log("es");
 //Este es un cuarto comentario
-console.log("secuencial");
+//console.log("secuencial");
+let palabra1: string = "Este";
+let palabra2: string = "algoritmo";
+let palabra3: string = "es";
+let palabra4: string = "secuencial";
+let resultado: string = console.log(palabra1, palabra2, palabra3, palabra4);
